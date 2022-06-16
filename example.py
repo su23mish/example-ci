529422fefc15f5e34a095dd0db2ad6162cc2d20a
su23mish <107569890+su23mish@users.noreply.github.com>
2022-06-16 07:22:10 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+def add(a,b):
+  return a + b
+
+def test_add():
+  assert add(2,3)== 5
+  assert add('space','ship') == 'spaceship'
+  
+def subtract(ab,b):
+  return a+b # this will be fixed
+
+
+# this will be implemented later
+#def test_subtract():
+# assert subtract(2,3) == -1
